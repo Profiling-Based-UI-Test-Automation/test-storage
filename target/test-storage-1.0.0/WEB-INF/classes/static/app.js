073e0288b36d41d9f16ebe3c42aa4393ba3fe264
heeseon <domich.hwang@gmail.com>
2018-09-24 22:22:38 +0900

    angularjs service 등록 및 filemodel directive를 위한 디버깅 코드 추가

--- target/test-storage-1.0.0/WEB-INF/classes/static/app.js
+++ target/test-storage-1.0.0/WEB-INF/classes/static/app.js
@@ -1,21 +1,47 @@
 var app = angular.module('archive', []);
 
 app.directive('fileModel', [ '$parse', function($parse) {
+  console.log('-------');
 	return {
 		restrict : 'A',
 		link : function(scope, element, attrs) {
 			var model = $parse(attrs.fileModel);
 			var modelSetter = model.assign;
+			console.log('model = ' + model);
+			console.log('modelSetter = ' + modelSetter);
 
 			element.bind('change', function() {
 				scope.$apply(function() {
 					modelSetter(scope, element[0].files[0]);
+					console.log('element[0].files[0] = ' + element[0].files[0]);
 				});
 			});
 		}
 	};
 } ]);
 
+app.service('ArchiveService', [ '$http', '$rootScope', function($http, $rootScope) {
+  
+  this.search = function() {
+    console.log("app.service... search...");
+    /*
+    var searchUrl = "/archive/documents";
+
+    $http.get(searchUrl, undefined, {
+      transformRequest : angular.identity,
+      headers : {
+        'Content-Type' : undefined
+      }
+    }).success(function(response) {
+      $rootScope.metadataList = response; 
+
+    }).error(function() {
+    });
+  */
+  }
+  
+  
+}]);
 
 app.service('fileUpload', ['$http','ArchiveService', function($http, ArchiveService) {
 	this.uploadFileToUrl = function(uploadUrl, apkfile, tcfile) {
@@ -50,11 +76,11 @@ app.controller('UploadCtrl', [ '$scope', 'fileUpload',
 				var apkfile = $scope.apkfile;
 				var tcfile = $scope.tcfile;
 				
-				console.log('file is ' + JSON.stringify(file));
+				console.log('apkfile is ' + JSON.stringify(apkfile));
 				console.log('tcfile is ' + JSON.stringify(tcfile));
 				
-				var uploadUrl = "/archive/upload";
-				fileUpload.uploadFileToUrl(uploadUrl, apkfile, tcfile);
+				//var uploadUrl = "/archive/upload";
+				//fileUpload.uploadFileToUrl(uploadUrl, apkfile, tcfile);
 			};
 		} 
 ]);
