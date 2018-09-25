98bfe6514953bd7ff88d507ee6ef7873a07f985c
heeseon <domich.hwang@gmail.com>
2018-09-25 12:52:02 +0900

    사용자 선택 화일 인식 안되는 문제 해결

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,92 +1,57 @@
-var app = angular.module('archive', []);
+'use strict';
+var App = angular.module('uploadTestFiles',[]);
 
-app.directive('fileModel', [ '$parse', function($parse) {
-  console.log('-------');
-	return {
-		restrict : 'A',
-		link : function(scope, element, attrs) {
-			var model = $parse(attrs.fileModel);
-			var modelSetter = model.assign;
-			console.log('model = ' + model);
-			console.log('modelSetter = ' + modelSetter);
+App.factory('docService', ['$http', '$q', 'urls', function ($http, $q, urls) {
 
-			element.bind('change', function() {
-				scope.$apply(function() {
-					modelSetter(scope, element[0].files[0]);
-					console.log('element[0].files[0] = ' + element[0].files[0]);
-				});
-			});
-		}
-	};
+            var factory = {
+                saveAppTestFiles: saveAppTestFiles
+            };
+
+            return factory;
+
+            function saveAppTestFiles(apkfile, tcfile) {
+              console.log("apkfile = " + apkfile);
+              console.log("tcfile = " + tcfile);
+            };
+
+        }
+    ]);
+App.controller('uploadController',
+    ['$scope', '$rootScope','docService','$http', function($scope, $rootScope, docService, $http) {
+
+        $scope.apkfile = '';
+        $scope.tcfile = '';
+
+        $scope.upload = function(){
+            var apkfile = $scope.apkfile;
+            var tcfile = $scope.tcfile;
+            console.log('apkfile = ' + apkfile);
+            console.log('tcfile = ' + tcfile);
+            docService.saveAppTestFiles(apkfile, tcfile);
+            
+        }
+    }
+    ]);
+
+App.constant('urls', {
+    DOC_URL: 'http://localhost:8080/doc/'
+});
+
+App.directive('fileModel', [ '$parse', function($parse) {
+    return {
+        restrict : 'A',
+        link : function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function() {
+                scope.$apply(function() {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
 } ]);
 
-app.service('ArchiveService', [ '$http', '$rootScope', function($http, $rootScope) {
-  
-  this.search = function() {
-    console.log("app.service... search...");
-    /*
-    var searchUrl = "/archive/documents";
-
-    $http.get(searchUrl, undefined, {
-      transformRequest : angular.identity,
-      headers : {
-        'Content-Type' : undefined
-      }
-    }).success(function(response) {
-      $rootScope.metadataList = response; 
-
-    }).error(function() {
-    });
-  */
-  }
-  
-  
-}]);
-
-app.service('fileUpload', ['$http','ArchiveService', function($http, ArchiveService) {
-	this.uploadFileToUrl = function(uploadUrl, apkfile, tcfile) {
-		
-		if(apkfile == undefined){
-			window.alert("apkfile 를 입력해 주세요~");
-			return;
-		}
-		if(tcfile == undefined){
-			window.alert("tcfile  입력해 주세요~");
-			return;
-		}
-		else {
-		  if((apkfile.name).search("\apk" ) == -1){
-        window.alert("application을 apk file format으로 올려 주세요~");
-        return;
-      }
-		  
-			if((tcfile.name).search("\.zip" ) == -1){
-				window.alert("tc를 zip file format으로 올려 주세요~");
-				return;
-			}
-		}
-		
-	}
-} ]);
-
-app.controller('UploadCtrl', [ '$scope', 'fileUpload',
-		function($scope, fileUpload) {
-			console.log("file upload");
-			$scope.uploadFile = function() {
-				var apkfile = $scope.apkfile;
-				var tcfile = $scope.tcfile;
-				
-				console.log('apkfile is ' + JSON.stringify(apkfile));
-				console.log('tcfile is ' + JSON.stringify(tcfile));
-				
-				//var uploadUrl = "/archive/upload";
-				//fileUpload.uploadFileToUrl(uploadUrl, apkfile, tcfile);
-			};
-		} 
-]);
-
-
-app.run(function($rootScope, ArchiveService) {
-	console.log("app.run");
-
+App.run(function($rootScope, $http) {
 });
