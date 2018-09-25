9feb667b40c760efd59892c809f7233be63066c3
heeseon <domich.hwang@gmail.com>
2018-09-25 20:32:02 +0900

    cors 적용

---
+++ src/main/java/teststorage/init/CORSConfig.java
@@ -0,0 +1,25 @@
+package teststorage.init;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/*
+ * springboot cors 설정 
+ */
+
+@Configuration
+public class CORSConfig {
+	@Bean 
+	public WebMvcConfigurer corsConfigurer() { 
+		return new WebMvcConfigurerAdapter() { 
+			public void addCorsMappings(CorsRegistry registry) { 
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080")
+                .allowedMethods("GET", "DELETE", "PUT", "POST", "OPTIONS");
+			} 
+		}; 
+	}
+}
