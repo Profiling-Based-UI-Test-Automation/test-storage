562a1e7b9d89a247b7e9d58c3b58e5685aed7f23
heeseon <domich.hwang@gmail.com>
2018-11-27 21:07:07 +0900

    maven 기반 프로젝트 gradlew 기반 프로젝트로 전환

---
+++ src/main/java/teststorage/WebStaticApplication.java
@@ -0,0 +1,48 @@
+package teststorage;
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebStaticApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebStaticApplication.class);
+	}
+	
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		// file load multipart 설정 - max file size 
+        factory.setMaxFileSize("1024MB");
+        factory.setMaxRequestSize("1024MB");
+        return factory.createMultipartConfig();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStaticApplication.class, args);
+	}
+
+}
